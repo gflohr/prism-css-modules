90166e883948fc90d25149f1398334f54fcf7f31
Guido Flohr <guido.flohr@cantanea.com>
2018-05-10 16:33:32 +0300

    syntax highlighting with PrismJS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 require('bootstrap/dist/css/bootstrap.css');
+
+require('./css/prism.css');
+require('prismjs');
+require('prismjs/components/prism-clike');
+require('prismjs/components/prism-markup-templating');
+require('prismjs/components/prism-tt2');
+require('prismjs/plugins/line-numbers/prism-line-numbers');

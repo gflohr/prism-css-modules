80f5ec45614a7ef493c4f5918b51a9ff139f645b
Guido Flohr <guido.flohr@cantanea.com>
2018-05-10 16:16:12 +0300

    styling with bootstrap 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-        entry: './src/index.js',
-        devServer: {
-                contentBase: './dist'
-        },
-        plugins: [
-                new CleanWebpackPlugin(['dist']),
-                new HtmlWebpackPlugin({
-                        title: 'PrismJS With CSS-Modules',
-                        template: 'index.html'
-                })
-        ],
-        output: {
-                filename: '[name].bundle.js',
-                path: path.resolve(__dirname, 'dist')
-        }
+    entry: './src/index.js',
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    "css-loader"
+                ] 
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'PrismJS With CSS-Modules',
+            template: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.[hash].css',
+            chunkFilename: "[id].css"
+        })
+    ],
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
